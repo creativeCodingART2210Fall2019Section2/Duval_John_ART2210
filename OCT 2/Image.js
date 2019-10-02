fb5bc00ae1403b826d0a0054a8d209ea25d1f7b5
John Duval <jduval7@lsu.edu>
2019-10-02 15:06:27 -0500

    up

--- OCT 2/Image.js
+++ OCT 2/Image.js
@@ -2,16 +2,19 @@ var img;
 
 function preload(){
     img=loadImage("https://github.com/jduval7/Duval_John_ART2210/raw/master/Image/Trees.jpg");
-    
+    soundFormats('mp3');
+    mySound = loadSound('Chill_Day.mp3');
 }
 
 function setup(){
         createCanvas(windowWidth,windowHeight);
-  
+        mySound.setVolume(0.1);
+        mySound.play();
 }
 
 
 function draw(){
+background(150,0,150);
   image(img, 50, 50);
   
 }
