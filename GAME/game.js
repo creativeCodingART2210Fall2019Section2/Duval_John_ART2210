4c6457c6d43485395a900f80fe367da43e15cd5b
John Duval <jduval7@lsu.edu>
2019-10-20 16:07:26 -0500

    Update

---
+++ GAME/game.js
@@ -0,0 +1,185 @@
+var pele;
+var players = [];
+var ball = [];
+let field;
+let peleimg = [];
+let opponent = [];
+let soccerball;
+let goal;
+var score = 0;
+var blocks = 0;
+let winnerpic;
+let loserpic;
+var goalie;
+var goalieimg = [];
+
+function preload() {
+    field = loadImage('soccerfield.jpg');
+    soccerball = loadImage('ball.png');
+    goal = loadImage('goal.png');
+    winnerpic = loadImage('winner.jpg');
+    loserpic = loadImage('loser.jpg');
+    for(let i = 0; i < 6; i++){
+        peleimg[i] = loadImage('player/run' + i + '.png');
+    }
+
+    for(let k = 0; k < 7; k++){
+        goalieimg[k] = loadImage('goalie/goalie' + k + '.png');
+    }
+
+    for(let j = 0; j < 9; j++){
+        opponent[j] = loadImage('opponent/op' + j + '.png');
+    }
+}
+
+
+function setup() {
+    frameRate(30);
+    createCanvas(windowWidth, windowHeight);
+    goalie = new Goalie(width/3, 10);
+    pele = new Pele();
+    for (var i = 0; i < 6; i++) {
+        players[i] = new Players(i*windowWidth/8 + 100, windowHeight/8);
+    }
+    for (var k = 0; k < 1; k++) {
+        goalie[k] = new Goalie(width/3, 10);
+    }
+  }
+  
+  
+  function draw() {
+        image(field, 0, 0);
+        field.resize(windowWidth, windowHeight);
+        image(goal, windowWidth/2.5, -50);
+        pele.show();
+        pele.move();
+        goalie.show();
+        goalie.move(); 
+        
+
+        blockBoard();
+        scoreBoard();
+        
+        gameOver();
+     //   image(peleimg[frameCount % peleimg.length], 100, 100);
+      //  image(opponent[0], 250, 250);
+        
+        for (var i = 0; i < ball.length; i++) {
+            ball[i].show();
+            ball[i].move();
+            for(var j = 0; j < players.length; j++) {
+                if(ball[i].hits(players[j])) {
+                    players[i].booyah();
+                    ball[i].remove();
+                    blocks += 1;
+                   // ball[i].deflect();
+                }  else if (ball[i].y < 10 && ball[i].x < windowWidth/1.5  && ball[i].x > windowWidth/2.5) {
+                    score += 1;
+                    ball[i].remove();
+                }
+            }
+        }
+
+        for (var i = 0; i < players.length; i++) {
+            players[i].show();
+            players[i].move();
+
+            if(players[i].x > windowWidth-100) {
+                players[i].xdir = random(-5,-7); 
+            } else if(players[i].y > windowHeight/2.5) {
+                players[i].ydir = random(-5,-7);
+            } else if(players[i].y < 50) {
+                players[i].ydir = random(5,7);
+            } else if(players[i].x < 50) {
+                players[i].xdir = random(5,7); 
+            }
+        }
+
+        for (var i = ball.length-1; i >=0; i--) {
+            if(ball[i].toDelete) {
+                ball.splice(i, 1);
+            }
+        }
+
+  }
+
+
+  function windowResized(){
+    resizeCanvas(windowWidth,windowHeight); 
+    field.resize(windowWidth, windowHeight);
+}
+
+function keyReleased() {
+    pele.setDir(0);
+    pele.setyDir(0);
+}
+
+function keyPressed() {
+    if (key === ' ') {
+        var shot = new Ball(pele.x, pele.y);
+        ball.push(shot);
+    }
+
+
+    if (keyCode === RIGHT_ARROW) {
+        pele.setDir(1);
+    }  else if (keyCode === LEFT_ARROW) {
+        pele.setDir(-1);
+    }   else if (keyCode === UP_ARROW) {
+        pele.setyDir(-1); }
+        else if (keyCode === DOWN_ARROW) {
+            pele.setyDir(1); }
+}
+
+function scoreBoard() {
+    fill(50, 30, 240);
+    rect(70, 10, 170, 110);
+    textSize(100);
+    fill(200, 0, 180);
+    text(score, 100, 100);
+    textSize(25);
+    fill(200, 0, 180);
+    text('SCORE', 100, 150);
+}
+
+function blockBoard() {
+    fill(200, 75, 75);
+    rect(windowWidth/1.25, 10, 170, 110);
+    textSize(100);
+    fill(25, 200, 180);
+    text(blocks, windowWidth/1.195, 100);
+    textSize(25);
+    fill(25, 200, 180);
+    text('BLOCKS', windowWidth/1.22, 150);
+}
+
+function gameOver() {
+    var d = dist(players.x, players.y, pele.x, pele.y);
+    if(score > 100) {
+        winner();
+    } if(blocks > 5) {
+        loser();
+    } if(pele.y < windowHeight/2.5) {
+        loser();
+    }
+}
+
+function winner() {
+    background(0);
+    image(winnerpic, windowWidth/2.5,windowHeight/2.5);
+    textSize(100);
+    fill(100, 255, 250);
+    text('You got ' + score + ' Points!!!', windowWidth/5, windowHeight/3);
+}
+
+function loser() {
+    background(0);
+    image(loserpic, windowWidth/2.75,windowHeight/2.5);
+    textSize(100);
+    fill(100, 255, 250);
+    text('You got Pwned!!!', windowWidth/5, windowHeight/3);
+}
+
+
+  
+        
---
+++ GAME/gameobjects.js
@@ -0,0 +1,120 @@
+
+function Pele() {
+    this.x = width/2;
+    this.y = height-100; 
+    this.xdir = 0;
+    this.ydir = 0;
+    this.r = 50;
+
+    this.show = function() {
+        
+     //   image(peleimg[0], this.x, this.y);
+     image(peleimg[frameCount % peleimg.length], this.x, this.y);
+    }
+
+    this.setyDir = function(dir){
+        this.ydir = dir;
+    }
+
+    this.setDir = function(dir){
+        this.xdir = dir;
+        
+    }
+
+    this.move = function(dir) {
+        this.x += this.xdir*10;
+        this.y += this.ydir*10;
+    }
+}
+
+
+function Goalie(x, y) {
+    this.x = x;
+    this.y = y; 
+    this.r = 50;
+
+    this.xdir = random(2,3);
+    this.ydir = 0;
+
+    this.move = function() {
+        this.x = this.x + this.xdir;
+        this.y = this.y + this.ydir;
+
+        if(this.x > windowWidth/1.75) {
+            this.xdir = random(-2,-3);
+        }  else if(this.x < windowWidth/2.5) {
+            this.xdir = random(2,3);
+        }
+    }
+
+    this.show = function() {
+        image(goalieimg[frameCount % goalieimg.length], this.x, this.y);
+    }
+
+}
+
+
+
+
+function Players(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 50;
+
+    this.xdir = random(5,7);
+    this.ydir = random(5,7);
+
+    this.booyah = function() {
+        fill(255, 255, 255);
+        textSize(100);
+        text('BOOYAH!!!', width/2, height/2);
+    }
+
+    this.move = function() {
+        this.x = this.x + this.xdir;
+        this.y = this.y + this.ydir;
+    }
+
+    this.show = function() {
+        image(opponent[frameCount % peleimg.length], this.x, this.y);
+    }
+
+}
+
+
+
+function Ball(x, y) { 
+    this.x = x;
+    this.y = y;
+    this.r = 25;
+
+    this.show = function() {
+        image(soccerball, this.x, this.y);
+    }
+
+    this.remove = function() {
+        this.toDelete = true;
+    }
+
+    this.hits = function(players) {
+        var d = dist(this.x, this.y, players.x, players.y);
+        if (d < this.r + players.r) {
+            return true; 
+        } else {
+            return false;
+        }
+    }
+
+    this.saves = function(goalie) {
+        var d = dist(this.x, this.y, goalie.x, goalie.y);
+        if (d < this.r + goalie.r) {
+            return true; 
+        } else {
+            return false;
+        }
+    }
+
+    this.move = function() {
+        this.y = this.y -10;
+    }
+}
